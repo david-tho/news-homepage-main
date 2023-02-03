9c6f6f21029e19ec539780dedf475402275d72a2
rayzii-gh <117836000+rayzii-gh@users.noreply.github.com>
2023-02-03 23:38:36 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+const mainContainer = document.querySelector(".container");
+const headerEl = document.querySelector(".header");
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".nav__btn");
+const overlay = document.querySelector(".overlay");
 
+const navToggle = () => {
+  navBtn.classList.toggle("active");
+  nav.classList.toggle("active");
+  overlay.classList.toggle("hidden");
+};
+
+const overlayClick = () => {
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+    navBtn.classList.remove("active");
+    overlay.classList.add("hidden");
+  }
+};
+
+navBtn.addEventListener("click", navToggle);
+overlay.addEventListener("click", overlayClick);
